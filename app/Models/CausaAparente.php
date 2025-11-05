1b3bc9ff36801e62b7ba19725f2afab36425d365
mutoLucas <lucasfbr123@gmail.com>
2025-11-04 22:58:29 -0300

    DB:add models

---
+++ app/Models/CausaAparente.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CausaAparente extends Model
+{
+    protected $table = 'tbl_CausaAparente';
+    public $timestamps = false;
+}
--- app/Models/Parada.php
+++ app/Models/Parada.php
@@ -9,6 +9,31 @@ use Illuminate\Database\Eloquent\Model;
 class Parada extends Model
 {
     protected $table = 'tbl_Paradas';
+    protected $primaryKey = 'Id';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'Id',
+        'Validade',
+        'Producao',
+        'Sistema',
+        'Equipamento',
+        'DataInicio',
+        'DataFim',
+        'Duracao',
+        'EqpGerador',
+        'TipoCodigo',
+        'GrupoCodigo',
+        'CodigoFalha',
+        'Turno',
+        'CausaAparente',
+        'Operador',
+        'Observacao',
+        'Componente',
+        'ModoFalha',
+        'Apropriador',
+        'NumeroParada'
+    ];
 
     public function getDataInicialAttribute()
     {
